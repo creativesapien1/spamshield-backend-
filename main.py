3b093bdc89870cda5895c8b1c88bb836ee2b869e
Your Name <youremail@example.com>
2026-03-01 18:09:35 +0530

    Fix: CORS allow credentials

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 @app.get("/")
 def root():
     return {
